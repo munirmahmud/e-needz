8e308ac217cacca90c0018fed030c504e3c74f03
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-11 10:39:30 +0600

    OTP verification page done

---
+++ pages/account/confirm-registration.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PageContainer from "~/components/layouts/PageContainer";
+import UserConfirmation from "~/components/partials/account/UserConfirmation";
+import FooterFullwidth from "~/components/shared/footers/FooterFullwidth";
+
+const ConfirmRegistration = () => {
+  const breadCrumb = [
+    {
+      text: "Home",
+      url: "/",
+    },
+    {
+      text: "OTP Validation",
+    },
+  ];
+
+  return (
+    <>
+      <PageContainer footer={<FooterFullwidth />} title="Login">
+        <div className="ps-page--my-account">
+          <UserConfirmation />
+        </div>
+      </PageContainer>
+    </>
+  );
+};
+
+export default ConfirmRegistration;
