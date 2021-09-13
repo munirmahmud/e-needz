4f88eedd6331bd0549d80c38f1357b0c28dddd52
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-13 19:15:56 +0600

    imported questions

--- components/elements/detail/description/DefaultDescription.jsx
+++ components/elements/detail/description/DefaultDescription.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
+import { Tabs } from "antd";
+import React from "react";
+import PartialDescription from "~/components/elements/detail/description/PartialDescription";
+import PartialReview from "~/components/elements/detail/description/PartialReview";
+import QuestionsAnswers from "~/components/elements/detail/description/QuestionsAnswers";
 
-import { Tabs } from 'antd'
-import PartialDescription from '~/components/elements/detail/description/PartialDescription'
-import PartialSpecification from '~/components/elements/detail/description/PartialSpecification'
-import PartialVendor from '~/components/elements/detail/description/PartialVendor'
-import PartialReview from '~/components/elements/detail/description/PartialReview'
-import PartialOffer from '~/components/elements/detail/description/PartialOffer'
-
-const { TabPane } = Tabs
+const { TabPane } = Tabs;
 
 const DefaultDescription = () => {
   return (
-    <div className='ps-product__content ps-tab-root'>
-      <Tabs defaultActiveKey='1'>
-        <TabPane tab='Description' key='1'>
+    <div className="ps-product__content ps-tab-root">
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Description" key="1">
           <PartialDescription />
         </TabPane>
-        <TabPane tab='Feedback (1)' key='2'>
+        <TabPane tab="Feedback (1)" key="2">
           <PartialReview />
         </TabPane>
-        <TabPane tab='Questions' key='3'>
+        <TabPane tab="Questions" key="3">
           <QuestionsAnswers />
         </TabPane>
-        <TabPane tab='Refund Policy' key='4'>
+        <TabPane tab="Refund Policy" key="4">
           <RefundPolicy />
         </TabPane>
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default DefaultDescription
+export default DefaultDescription;
