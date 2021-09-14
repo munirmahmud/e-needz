d722dd4b840ace75ebc4e5c1b7e6fd4fe1054b18
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-14 15:17:56 +0600

    Products of store component added

--- components/elements/detail/description/PartialDescription.jsx
+++ components/elements/detail/description/PartialDescription.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 
 const PartialDescription = ({ product_id, category_id }) => {
-  console.log(product_id, category_id);
   const [prodDescription, setProdDescription] = useState(undefined);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/product_description`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         product_id: product_id,
         category_id: category_id,
@@ -23,6 +22,7 @@ const PartialDescription = ({ product_id, category_id }) => {
 
   return (
     <div className="ps-document">
+      <h4>Product Description</h4>
       <div
         className="content"
         dangerouslySetInnerHTML={{ __html: prodDescription }}
---
+++ components/elements/detail/description/RefundPolicy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RefundPolicy = () => {
+  return (
+    <div className="ps-block--questions-answers">
+      <h4>Refund Policy</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nulla nemo
+        dolorum eius voluptatem nobis rerum aperiam nihil labore beatae,
+        voluptate libero? Aspernatur, quia possimus voluptates iste tempore
+        voluptatem numquam?
+      </p>
+    </div>
+  );
+};
+
+export default RefundPolicy;
