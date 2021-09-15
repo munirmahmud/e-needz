acbb1f1295e9954dbc098cac517eb0e743063987
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-15 16:26:15 +0600

    On sale price updated

--- components/elements/detail/modules/ModuleDetailTopInformation.jsx
+++ components/elements/detail/modules/ModuleDetailTopInformation.jsx
@@ -1,42 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
-import Rating from '~/components/elements/Rating'
+import Link from "next/link";
+import React from "react";
+import Rating from "~/components/elements/Rating";
 
 const ModuleDetailTopInformation = ({ product }) => {
   // Views
-  let priceView
-  if (product.on_sale === '1') {
+  let priceView;
+  if (product.on_sale === "1") {
     priceView = (
-      <h4 className='ps-product__price sale'>
-        <del style={{ color: 'red' }}>{product.price}</del>
-        <span className='font-weight-bold ml-2'>{product.offer_price} TK</span>
+      <h4 className="ps-product__price sale">
+        <del style={{ color: "#aeaeae" }}>{product.price}</del>
+        <span className="font-weight-bold ml-2">{product.offer_price} TK</span>
       </h4>
-    )
+    );
   } else {
-    priceView = <h4 className='ps-product__price'>{product.price} TK</h4>
+    priceView = <h4 className="ps-product__price">{product.price} TK</h4>;
   }
   return (
     <header>
       <h1>{product.title}</h1>
-      <div className='ps-product__meta'>
+      <div className="ps-product__meta">
         {product.brand_name !== null ? (
           <p>
             Brand:
             <Link href={`brands/${product.brand_id}`}>
-              <a className='ml-2 text-capitalize'>{product.brand_name}</a>
+              <a className="ml-2 text-capitalize">{product.brand_name}</a>
             </Link>
           </p>
         ) : (
-          ''
+          ""
         )}
-        <div className='ps-product__rating'>
+        <div className="ps-product__rating">
           <Rating />
           <span>(* review)</span>
         </div>
       </div>
       {priceView}
     </header>
-  )
-}
+  );
+};
 
-export default ModuleDetailTopInformation
+export default ModuleDetailTopInformation;
