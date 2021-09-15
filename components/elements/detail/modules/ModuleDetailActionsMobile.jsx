374b2ce7d08dd7ba3d8e7d1b43e45ea31b4b225d
mdabhn <mdabhn@gmail.com>
2021-09-15 11:22:56 +0600

    For monir vai

--- components/elements/detail/modules/ModuleDetailActionsMobile.jsx
+++ components/elements/detail/modules/ModuleDetailActionsMobile.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from 'react';
-import { connect, useDispatch } from 'react-redux';
-import useEcomerce from '~/hooks/useEcomerce';
+import React, { useState } from 'react'
+import { connect, useDispatch } from 'react-redux'
+import useEcomerce from '~/hooks/useEcomerce'
 
 const ModuleDetailActionsMobile = ({ ecomerce, product }) => {
-    const { addItem } = useEcomerce();
-    const handleAddItemToCart = (e) => {
-        e.preventDefault();
-        addItem(
-            { id: product.id, category_id: product.category_id, quantity: 1 },
-            ecomerce.cartItems,
-            'cart'
-        );
-    };
+  const { addItem } = useEcomerce()
+  const handleAddItemToCart = (e) => {
+    e.preventDefault()
+    addItem(
+      { id: product.id, campaign_id: product.campaign_id, quantity: 1 },
+      ecomerce.cartItems,
+      'cart'
+    )
+  }
 
-    return (
-        <div className="ps-product__actions-mobile">
-            <a
-                className="ps-btn ps-btn--black"
-                href="#"
-                onClick={(e) => handleAddItemToCart(e)}>
-                Add to cart
-            </a>
-            <a
-                className="ps-btn"
-                href="#"
-                onClick={(e) => handleAddItemToCart(e)}>
-                Buy Now
-            </a>
-        </div>
-    );
-};
+  return (
+    <div className='ps-product__actions-mobile'>
+      <a
+        className='ps-btn ps-btn--black'
+        href='#'
+        onClick={(e) => handleAddItemToCart(e)}
+      >
+        Add to cart
+      </a>
+      <a className='ps-btn' href='#' onClick={(e) => handleAddItemToCart(e)}>
+        Buy Now
+      </a>
+    </div>
+  )
+}
 
-export default connect((state) => state)(ModuleDetailActionsMobile);
+export default connect((state) => state)(ModuleDetailActionsMobile)
