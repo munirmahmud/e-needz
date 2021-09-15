123ed5320ab69775b1390647d358ee3890a3d7ed
mdabhn <mdabhn@gmail.com>
2021-09-15 13:32:52 +0600

    search box issue
    Search box is creating error while typing.. Its resloved ...

--- components/shared/headers/modules/SearchHeader.jsx
+++ components/shared/headers/modules/SearchHeader.jsx
@@ -1,192 +1,192 @@
-import { Spin } from "antd";
-import Link from "next/link";
-import Router from "next/router";
-import React, { useEffect, useRef, useState } from "react";
-import ProductSearchResult from "~/components/elements/products/ProductSearchResult";
-import ProductRepository from "~/repositories/ProductRepository";
+import { Spin } from 'antd'
+import Link from 'next/link'
+import Router from 'next/router'
+import React, { useEffect, useRef, useState } from 'react'
+import ProductSearchResult from '~/components/elements/products/ProductSearchResult'
+import ProductRepository from '~/repositories/ProductRepository'
 
 const exampleCategories = [
-  "All",
-  "Babies & Moms",
-  "Books & Office",
-  "Cars & Motocycles",
-  "Clothing & Apparel",
-  " Accessories",
-  "Bags",
-  "Kid’s Fashion",
-  "Mens",
-  "Shoes",
-  "Sunglasses",
-  "Womens",
-  "Computers & Technologies",
-  "Desktop PC",
-  "Laptop",
-  "Smartphones",
-  "Consumer Electrics",
-  "Air Conditioners",
-  "Accessories",
-  "Type Hanging Cell",
-  "Audios & Theaters",
-  "Headphone",
-  "Home Theater System",
-  "Speakers",
-  "Car Electronics",
-  "Audio & Video",
-  "Car Security",
-  "Radar Detector",
-  "Vehicle GPS",
-  "Office Electronics",
-  "Printers",
-  "Projectors",
-  "Scanners",
-  "Store & Business",
-  "Refrigerators",
-  "TV Televisions",
-  "4K Ultra HD TVs",
-  "LED TVs",
-  "OLED TVs",
-  "Washing Machines",
-  "Type Drying Clothes",
-  "Type Horizontal",
-  "Type Vertical",
-  "Garden & Kitchen",
-  "Cookware",
-  "Decoration",
-  "Furniture",
-  "Garden Tools",
-  "Home Improvement",
-  "Powers And Hand Tools",
-  "Utensil & Gadget",
-  "Health & Beauty",
-  "Equipments",
-  "Hair Care",
-  "Perfumer",
-  "Wine Cabinets",
-];
+  'All',
+  'Babies & Moms',
+  'Books & Office',
+  'Cars & Motocycles',
+  'Clothing & Apparel',
+  ' Accessories',
+  'Bags',
+  'Kid’s Fashion',
+  'Mens',
+  'Shoes',
+  'Sunglasses',
+  'Womens',
+  'Computers & Technologies',
+  'Desktop PC',
+  'Laptop',
+  'Smartphones',
+  'Consumer Electrics',
+  'Air Conditioners',
+  'Accessories',
+  'Type Hanging Cell',
+  'Audios & Theaters',
+  'Headphone',
+  'Home Theater System',
+  'Speakers',
+  'Car Electronics',
+  'Audio & Video',
+  'Car Security',
+  'Radar Detector',
+  'Vehicle GPS',
+  'Office Electronics',
+  'Printers',
+  'Projectors',
+  'Scanners',
+  'Store & Business',
+  'Refrigerators',
+  'TV Televisions',
+  '4K Ultra HD TVs',
+  'LED TVs',
+  'OLED TVs',
+  'Washing Machines',
+  'Type Drying Clothes',
+  'Type Horizontal',
+  'Type Vertical',
+  'Garden & Kitchen',
+  'Cookware',
+  'Decoration',
+  'Furniture',
+  'Garden Tools',
+  'Home Improvement',
+  'Powers And Hand Tools',
+  'Utensil & Gadget',
+  'Health & Beauty',
+  'Equipments',
+  'Hair Care',
+  'Perfumer',
+  'Wine Cabinets',
+]
 
 function useDebounce(value, delay) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState(value)
   useEffect(() => {
     // Update debounced value after delay
     const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+      setDebouncedValue(value)
+    }, delay)
 
     return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
+      clearTimeout(handler)
+    }
+  }, [value, delay])
 
-  return debouncedValue;
+  return debouncedValue
 }
 
 const SearchHeader = () => {
-  const inputEl = useRef(null);
-  const [isSearch, setIsSearch] = useState(false);
-  const [keyword, setKeyword] = useState("");
-  const [resultItems, setResultItems] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const debouncedSearchTerm = useDebounce(keyword, 300);
+  const inputEl = useRef(null)
+  const [isSearch, setIsSearch] = useState(false)
+  const [keyword, setKeyword] = useState('')
+  const [resultItems, setResultItems] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const debouncedSearchTerm = useDebounce(keyword, 300)
 
   function handleClearKeyword() {
-    setKeyword("");
-    setIsSearch(false);
-    setLoading(false);
+    setKeyword('')
+    setIsSearch(false)
+    setLoading(false)
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    Router.push(`/search?keyword=${keyword}`);
+    e.preventDefault()
+    Router.push(`/search?keyword=${keyword}`)
   }
 
   useEffect(() => {
     if (debouncedSearchTerm) {
-      setLoading(true);
+      setLoading(true)
       if (keyword) {
         const queries = {
           per_page: 10,
           page_offset: 0,
           product_name: keyword,
-          category_id: category_id ? category_id : "",
-        };
-        const products = ProductRepository.getSearchRecords(queries);
+          // category_id: category_id ? category_id : '',
+        }
+        const products = ProductRepository.getSearchRecords(queries)
         products.then((result) => {
-          setLoading(false);
-          setResultItems(result);
-          setIsSearch(true);
-        });
+          setLoading(false)
+          setResultItems(result)
+          setIsSearch(true)
+        })
       } else {
-        setIsSearch(false);
-        setKeyword("");
+        setIsSearch(false)
+        setKeyword('')
       }
       if (loading) {
-        setIsSearch(false);
+        setIsSearch(false)
       }
     } else {
-      setLoading(false);
-      setIsSearch(false);
+      setLoading(false)
+      setIsSearch(false)
     }
-  }, [debouncedSearchTerm]);
+  }, [debouncedSearchTerm])
 
   // Views
   let productItemsView,
     clearTextView,
     selectOptionView,
     loadingView,
-    loadMoreView;
+    loadMoreView
   if (!loading) {
     if (resultItems && resultItems.length > 0) {
       if (resultItems.length > 5) {
         loadMoreView = (
-          <div className="ps-panel__footer text-center">
-            <Link href="/search">
+          <div className='ps-panel__footer text-center'>
+            <Link href='/search'>
               <a>See all results</a>
             </Link>
           </div>
-        );
+        )
       }
       productItemsView = resultItems.map((product) => (
         <ProductSearchResult product={product} key={product.id} />
-      ));
+      ))
     } else {
-      productItemsView = <p>No product found.</p>;
+      productItemsView = <p>No product found.</p>
     }
-    if (keyword !== "") {
+    if (keyword !== '') {
       clearTextView = (
-        <span className="ps-form__action" onClick={handleClearKeyword}>
-          <i className="icon icon-cross2"></i>
+        <span className='ps-form__action' onClick={handleClearKeyword}>
+          <i className='icon icon-cross2'></i>
         </span>
-      );
+      )
     }
   } else {
     loadingView = (
-      <span className="ps-form__action">
-        <Spin size="small" />
+      <span className='ps-form__action'>
+        <Spin size='small' />
       </span>
-    );
+    )
   }
 
   selectOptionView = exampleCategories.map((option) => (
     <option value={option} key={option}>
       {option}
     </option>
-  ));
+  ))
 
   return (
     <form
-      className="ps-form--quick-search"
-      method="get"
-      action="/"
+      className='ps-form--quick-search'
+      method='get'
+      action='/'
       onSubmit={handleSubmit}
     >
-      <div className="ps-form__categories">
-        <select className="form-control">{selectOptionView}</select>
+      <div className='ps-form__categories'>
+        <select className='form-control'>{selectOptionView}</select>
       </div>
-      <div className="ps-form__input">
+      <div className='ps-form__input'>
         <input
           ref={inputEl}
-          className="form-control"
-          type="text"
+          className='form-control'
+          type='text'
           value={keyword}
           placeholder="I'm shopping for..."
           onChange={(e) => setKeyword(e.target.value)}
@@ -195,12 +195,12 @@ const SearchHeader = () => {
         {loadingView}
       </div>
       <button onClick={handleSubmit}>Search</button>
-      <div className={`ps-panel--search-result${isSearch ? " active " : ""}`}>
-        <div className="ps-panel__content">{productItemsView}</div>
+      <div className={`ps-panel--search-result${isSearch ? ' active ' : ''}`}>
+        <div className='ps-panel__content'>{productItemsView}</div>
         {loadMoreView}
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SearchHeader;
+export default SearchHeader
