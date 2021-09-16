a7c2c387c06cfe911ac318664e6bb859af04bdec
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-16 12:27:59 +0600

    Questions & ans modified

--- components/elements/detail/description/QuestionsAnswers.jsx
+++ components/elements/detail/description/QuestionsAnswers.jsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const QuestionsAnswers = () => {
   return (
     <div className="ps-document">
       <h5>Questions about this product (3)</h5>
+      <p>
+        <Link href="/account/login">
+          <a style={{ color: "#fd8b01" }}>Login</a>
+        </Link>{" "}
+        or{" "}
+        <Link href="/account/register">
+          <a style={{ color: "#fd8b01" }}>Register</a>
+        </Link>{" "}
+        to ask questions
+      </p>
       <form>
         <div className="form-group d-flex">
           <label htmlFor="question" className="sr-only">
