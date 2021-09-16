b4e1aa039b03ddd96b3148c8707e8363c7f9dd41
mdabhn <mdabhn@gmail.com>
2021-09-16 12:08:17 +0600

    Top Categories $ Shop by Store $ Shop By Brand Page Added...

---
+++ pages/brand/index.jsx
@@ -0,0 +1,128 @@
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import PageContainer from '~/components/layouts/PageContainer'
+import BreadCrumb from '~/components/elements/BreadCrumb'
+
+const index = () => {
+  const [catProds, setCatProducts] = useState()
+  const [filterProds, setFilterProds] = useState()
+
+  const filterOut = (tag) => {
+    const newProd = catProds.filter((prod) =>
+      prod.brand_name.toLowerCase().includes('' + tag)
+    )
+    setFilterProds(newProd)
+  }
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/brand_list`, {
+      method: 'post',
+      body: JSON.stringify({
+        per_page: '100',
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCatProducts(data.data)
+        setFilterProds(data.data)
+      })
+  }, [])
+
+  const breadCrumb = [
+    {
+      text: 'Home',
+      url: '/',
+    },
+    {
+      text: 'All Brands',
+      url: '/brand',
+    },
+  ]
+
+  return (
+    <PageContainer>
+      <BreadCrumb breacrumb={breadCrumb} />
+      <div className='ps-top-categories mt-4'>
+        <div className='ps-container'>
+          <div className='section-white'>
+            <div className='section__header'>
+              <div className='ps-block--countdown-deal'>
+                <div className='ps-block__left'>
+                  <h3>All Brands</h3>
+                </div>
+              </div>
+
+              <div className='search-products'>
+                <div className='ps-form__input'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    placeholder='Search...'
+                    onChange={(e) => {
+                      filterOut(e.target.value)
+                    }}
+                  />
+                </div>
+                <button className='ps-btn'>
+                  <img
+                    src='/static/icons/magnifiying-glass.svg'
+                    alt='Search Products'
+                  />
+                </button>
+                {/* <Link href={_link}>
+                <a className='ps-btn view-all'>View all</a>
+              </Link> */}
+              </div>
+            </div>
+
+            <div className='row'>
+              {filterProds ? (
+                filterProds.length > 0 ? (
+                  filterProds.map((data, id) => {
+                    return (
+                      <div
+                        className='col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6'
+                        key={id}
+                      >
+                        <div className='ps-block--category'>
+                          <Link
+                            href={`top-categories/${
+                              data.category_id
+                                ? data.category_id
+                                : data.brand_id
+                                ? data.brand_id
+                                : data.seller_id
+                            }`}
+                          >
+                            <a className='ps-block__overlay'></a>
+                          </Link>
+                          <img
+                            src={`/static/img/categories/5.jpg`}
+                            alt='E-needz'
+                          />
+                          <p>
+                            {data.category_name
+                              ? data.category_name
+                              : data.seller_store_name}
+
+                            {data.brand_name ? data.brand_name : ''}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })
+                ) : (
+                  <p style={{ marginLeft: '10px' }}>No product(s) found.</p>
+                )
+              ) : (
+                ''
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
+  )
+}
+
+export default index
