9d2a2fde087ba9ad1fe97e38842d03c9b41a854e
Munir Mahmud <munirmahmud6@gmail.com>
2021-09-20 15:49:47 +0600

    Search page design done

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,92 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import BreadCrumb from '~/components/elements/BreadCrumb'
-import Product from '~/components/elements/products/Product'
-import ProductGroupGridItems from '~/components/partials/product/ProductGroupGridItems'
-import PageContainer from '~/components/layouts/PageContainer'
-import Newsletters from '~/components/partials/commons/Newletters'
-import useGetProducts from '~/hooks/useGetProducts'
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import BreadCrumb from "~/components/elements/BreadCrumb";
+import ProductDealOfDay from "~/components/elements/products/ProductDealOfDay";
+import PageContainer from "~/components/layouts/PageContainer";
+import ProductGroupGridItems from "~/components/partials/product/ProductGroupGridItems";
+import useGetProducts from "~/hooks/useGetProducts";
 
 const SearchPage = () => {
-  const [pageSize] = useState(100)
-  const [keyword, setKeyword] = useState('')
-  const { productItems, loading, getProducts } = useGetProducts()
-  const Router = useRouter()
-  const { query } = Router
+  const [pageSize] = useState(100);
+  const [keyword, setKeyword] = useState("");
+  const { productItems, loading, getProducts } = useGetProducts();
+  const Router = useRouter();
+  const { query } = Router;
 
   function handleSetKeyword() {
-    if (query && query.keyword !== '') {
-      setKeyword(query.keyword)
+    if (query && query.keyword !== "") {
+      setKeyword(query.keyword);
     } else {
-      setKeyword('')
+      setKeyword("");
     }
   }
 
   useEffect(() => {
     if (query && query.keyword) {
-      handleSetKeyword(query.keyword)
+      handleSetKeyword(query.keyword);
       const queries = {
         _limit: pageSize,
         title_contains: query.keyword,
-      }
-      getProducts(queries)
+      };
+      getProducts(queries);
     }
-  }, [query])
+  }, [query]);
 
   const breadcrumb = [
     {
-      text: 'Home',
-      url: '/',
+      text: "Home",
+      url: "/",
     },
     {
-      text: 'Search Result',
+      text: "Search Result",
     },
-  ]
+  ];
 
-  let shopItemsView, statusView
+  let shopItemsView, statusView;
 
   if (!loading) {
     if (productItems) {
-      shopItemsView = <ProductGroupGridItems columns={6} pageSize={pageSize} />
+      shopItemsView = <ProductGroupGridItems columns={6} pageSize={pageSize} />;
       if (productItems.length > 0) {
         const items = productItems.map((item) => {
           return (
-            <div className='col-md-3 col-sm-6 col-6' key={item.id}>
-              <Product product={item} />
+            <div className="col-xl-2 col-lg-3 col-sm-3 col-6" key={item.id}>
+              <ProductDealOfDay product={item} />
             </div>
-          )
-        })
-        shopItemsView = <div className='ps-product-items row'>{items}</div>
+          );
+        });
+        shopItemsView = <div className="ps-product-items row">{items}</div>;
         statusView = (
           <p>
-            <strong style={{ color: '#000' }}>{productItems.length}</strong>{' '}
+            <strong style={{ color: "#000" }}>{productItems.length}</strong>{" "}
             record(s) found.
           </p>
-        )
+        );
       } else {
-        shopItemsView = <p>No product(s) found.</p>
+        shopItemsView = <p>No product(s) found.</p>;
       }
     } else {
-      shopItemsView = <p>No product(s) found.</p>
+      shopItemsView = <p>No product(s) found.</p>;
     }
   } else {
-    statusView = <p>Searching...</p>
+    statusView = <p>Searching...</p>;
   }
 
   return (
     <PageContainer title={`Search results for: "${keyword}" `}>
-      <div className='ps-page'>
+      <div className="ps-page">
         <BreadCrumb breacrumb={breadcrumb} />
       </div>
-      <div className='container'>
-        <div className='ps-shop ps-shop--search'>
-          <div className='container'>
-            <div className='ps-shop__header'>
-              <h1>
+
+      <div className="ps-container mb-4">
+        <div className="ps-shop ps-shop--search">
+          <div className="ps-shop__header">
+            <div className="section-white">
+              <h1 className="mb-0">
                 Search result for: "<strong>{keyword}</strong>"
               </h1>
             </div>
-            <div className='ps-shop__content'>
+          </div>
+
+          <div className="section-white box-layout">
+            <div className="ps-shop__content ">
               {statusView}
               {shopItemsView}
             </div>
@@ -94,7 +97,7 @@ const SearchPage = () => {
         </div>
       </div>
     </PageContainer>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
