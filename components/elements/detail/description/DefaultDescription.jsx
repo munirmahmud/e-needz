00292cd3c78a830f86b19d65ef4b460ed26a6e7d
Munir Mahmud <munirmahmud6@gmail.com>
2021-10-11 10:28:43 +0600

    Review fixed

--- components/elements/detail/description/DefaultDescription.jsx
+++ components/elements/detail/description/DefaultDescription.jsx
@@ -1,43 +1,43 @@
-import { Tabs } from 'antd'
-import React from 'react'
-import PartialDescription from '~/components/elements/detail/description/PartialDescription'
-import PartialReview from '~/components/elements/detail/description/PartialReview'
-import QuestionsAnswers from '~/components/elements/detail/description/QuestionsAnswers'
-import RefundPolicy from '~/components/elements/detail/description/RefundPolicy'
+import { Tabs } from "antd";
+import React from "react";
+import PartialDescription from "~/components/elements/detail/description/PartialDescription";
+import PartialReview from "~/components/elements/detail/description/PartialReview";
+import QuestionsAnswers from "~/components/elements/detail/description/QuestionsAnswers";
+import RefundPolicy from "~/components/elements/detail/description/RefundPolicy";
 
-const { TabPane } = Tabs
+const { TabPane } = Tabs;
 
 const DefaultDescription = ({ product_id, category_id, seller_id }) => {
   return (
-    <div className='ps-product__content ps-tab-root'>
-      <Tabs defaultActiveKey='1'>
-        <TabPane tab='Description' key='1'>
+    <div className="ps-product__content ps-tab-root">
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Description" key="1">
           <PartialDescription
             product_id={product_id}
             category_id={category_id}
             seller_id={seller_id}
           />
         </TabPane>
-        <TabPane tab='Reviews (1)' key='4'>
+        <TabPane tab="Reviews (1)" key="4">
           <PartialReview
             product_id={product_id}
             category_id={category_id}
             seller_id={seller_id}
           />
         </TabPane>
-        <TabPane tab='Questions and Answers' key='5'>
+        <TabPane tab="Questions and Answers" key="5">
           <QuestionsAnswers
             product_id={product_id}
             category_id={category_id}
             seller_id={seller_id}
           />
         </TabPane>
-        <TabPane tab='Refund Policy' key='6'>
+        <TabPane tab="Refund Policy" key="6">
           <RefundPolicy />
         </TabPane>
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default DefaultDescription
+export default DefaultDescription;
